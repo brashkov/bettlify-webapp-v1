fcffcce5635238dc45ed41be8c63f0e5e6f73104
Bo <bobyyws978@gmail.com>
2024-12-29 15:06:19 +0200

    +added GTM

--- src/components/analytics/Analytics.tsx
+++ src/components/analytics/Analytics.tsx
@@ -1,42 +1,70 @@
 'use client'
 
 import Script from 'next/script'
-import { FACEBOOK_PIXEL_ID } from '@/lib/analytics-config'
+import { FACEBOOK_PIXEL_ID, GTM_ID } from '@/lib/analytics-config'
 import { useAnalytics } from '@/hooks/useAnalytics'
 
 export default function Analytics() {
   useAnalytics()
 
-  if (!FACEBOOK_PIXEL_ID) return null
-
   return (
     <>
+      {/* Google Tag Manager */}
       <Script
-        id="fb-pixel"
+        id="gtm"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${FACEBOOK_PIXEL_ID}');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
           `,
         }}
       />
+      
+      {/* Google Tag Manager (noscript) */}
       <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: 'none' }}
-          src={`https://www.facebook.com/tr?id=${FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
-          alt=""
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
         />
       </noscript>
+
+      {/* Facebook Pixel */}
+      {FACEBOOK_PIXEL_ID && (
+        <>
+          <Script
+            id="fb-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${FACEBOOK_PIXEL_ID}');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=${FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
+              alt=""
+            />
+          </noscript>
+        </>
+      )}
     </>
   )
 } 
--- src/lib/analytics-config.ts
+++ src/lib/analytics-config.ts
@@ -1,4 +1,5 @@
 export const FACEBOOK_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || ''
+export const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || 'GTM-T44H6XHJ'
 
 export const FB_EVENTS = {
   PAGEVIEW: 'PageView',
